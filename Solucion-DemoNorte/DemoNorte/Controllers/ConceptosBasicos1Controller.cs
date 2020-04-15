45b9148f2a90539dcd434c0c5ccd0eccd6eb5894
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-14 22:14:09 -0300

    Cargado primer mapeo

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/ConceptosBasicos1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class ConceptosBasicos1Controller : Controller
+    {
+        // GET: ConceptosBasicos1
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+    }
+}
