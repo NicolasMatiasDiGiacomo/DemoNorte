8a8de43108504d58c2924f8264370a5f42317274
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-15 15:09:16 -0300

    Ejercicio 2 resuelto

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/ConceptosBasicos2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class ConceptosBasicos2Controller : Controller
+    {
+        // GET: ConceptosBasicos2
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+    }
+}
