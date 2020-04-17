ec2cfc5ac4438bd5de03df3cc38b447c330bb86f
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-16 21:12:37 -0300

    Correccion del codigo HTML

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/ConceptosBasicos3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class ConceptosBasicos3Controller : Controller
+    {
+        // GET: ConceptosBasicos3
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+    }
+}
