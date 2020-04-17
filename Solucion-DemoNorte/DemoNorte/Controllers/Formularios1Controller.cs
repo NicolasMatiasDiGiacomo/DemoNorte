ff2a038aea7f279736f160327b19831b46a14e33
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-17 19:05:24 -0300

    Ejercicio 8

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/Formularios1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class Formularios1Controller : Controller
+    {
+        // GET: Formularios1
+        public ActionResult Ejercicio8()
+        {
+            return View();
+        }
+    }
+}
