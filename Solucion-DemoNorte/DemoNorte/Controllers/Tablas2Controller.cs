a8bf8bc8823ae9bb79e03a12c11762c343a7116f
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-17 18:02:46 -0300

    Ejercicio 7

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/Tablas2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class Tablas2Controller : Controller
+    {
+        // GET: Tablas2
+        public ActionResult Ejercicio7()
+        {
+            return View();
+        }
+    }
+}
