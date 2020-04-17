83ff5add782e6bba27587bee82692d4fea171f63
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-17 15:01:41 -0300

    Ejercicio 4 y 5

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/EnlacesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class EnlacesController : Controller
+    {
+        // GET: Enlaces
+        public ActionResult Ejercicio4()
+        {
+            return View();
+        }
+    }
+}
---
+++ Solucion-DemoNorte/DemoNorte/Controllers/ListasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class ListasController : Controller
+    {
+        // GET: Listas
+        public ActionResult Ejercicio5()
+        {
+            return View();
+        }
+    }
+}
