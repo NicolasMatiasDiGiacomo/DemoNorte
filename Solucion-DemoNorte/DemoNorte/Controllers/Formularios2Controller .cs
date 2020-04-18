17bde5fd46c22fe7be82fadf8a88d21f0b6f40a7
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-04-17 21:44:56 -0300

    Fin de los ejercicios

---
+++ Solucion-DemoNorte/DemoNorte/Controllers/Formularios2Controller .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoNorte.Controllers
+{
+    public class Formularios2Controller : Controller
+    {
+        // GET: Formularios2
+        public ActionResult Ejercicio9()
+        {
+            return View();
+        }
+    }
+}
